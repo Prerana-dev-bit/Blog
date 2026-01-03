2b6630bb9cffddaa45d1dc4e7eeb853b579a83b3
Prerana <preranapatil3045@gmail.com>
2026-01-03 13:37:26 +0530

    Initial Django blog project

---
+++ blogs/views.py
@@ -0,0 +1,39 @@
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.db.models import Q
+from .models import Blog,Category
+
+# Create your views here.
+def posts_by_category(request,category_id):
+    #fetch the posts thet belongs to the category with th id category_id
+    posts=Blog.objects.filter(status='Published',category=category_id)
+    # use try/except when we want to do some custom action if the category does not exist
+    # try:
+    #     category=Category.objects.get(pk=category_id)
+    # except:
+    #     #redirect the user to homepage
+    #     return redirect('home')
+#use get_object_404 when you want to show 404 error page if the category does not exists
+    category = get_object_or_404(Category, pk=category_id)
+
+    context = {
+        'posts':posts,
+        'category':category,
+    }
+    return render(request,'posts_by_category.html',context)
+
+def blogs(request,slug):
+    single_blog = get_object_or_404(Blog,slug=slug, status='Published')
+    context = {
+        'single_blog':single_blog,
+    }
+    return render(request, 'blogs.html',context)
+
+def search (request):
+    keyword = request.GET.get('keyword')
+    blogs = Blog.objects.filter(Q(title__icontains=keyword)|Q(short_description__icontains=keyword)|Q(blog_body__icontains=keyword),status='Published')
+    context = {
+        'blogs':blogs,
+        'keyword':keyword,
+    }
+    return render(request,'search.html',context)
