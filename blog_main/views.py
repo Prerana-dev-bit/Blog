01c3a1d9518411950bc09a9cc9856c83956d8fda
Prerana <preranapatil3045@gmail.com>
2026-01-05 09:56:23 +0530

    posts crud operations/features

--- blog_main/views.py
+++ blog_main/views.py
@@ -25,7 +25,7 @@ def home(request):
 
 def register(request):
     if request.method =='POST':
-        form= form = RegistrationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('register')
